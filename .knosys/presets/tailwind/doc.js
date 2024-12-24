029c7e8ffbd0a1b7f9c3fc99e5c194fc7908bc92
Ourai Lin <ourairyu@gmail.com>
2024-12-24 15:44:38 +0800

    fix: source file paths for Tailwind CSS while building doc site

--- .knosys/presets/tailwind/doc.js
+++ .knosys/presets/tailwind/doc.js
@@ -1,10 +1,12 @@
 const { nextui } = require('@nextui-org/react');
 const starlightPlugin = require('@astrojs/starlight-tailwind');
 
+const srcDirPath = 'src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './.knosys/sites/default/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    process.env.OB_SITE === 'DOC' ? `./${srcDirPath}` : `./.knosys/sites/default/${srcDirPath}`,
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
