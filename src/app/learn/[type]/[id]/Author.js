b62a431d1794a506d351b351b3cbc878d866eac7
huzhengen <huzhengen@163.com>
2024-12-31 14:29:49 +0800

    fix: ui & ux optimization

--- src/app/learn/[type]/[id]/Author.js
+++ src/app/learn/[type]/[id]/Author.js
@@ -24,11 +24,11 @@ export function Author({ data }) {
     <div className="flex justify-between pb-6 mt-6">
       <div className="flex items-center" suppressHydrationWarning>
         <Avatar size={56} user={data?.team_user} />
-        <div className="ml-4">
-          <h4 className="text-sm font-bold md:text-lg">
+        <div className="ml-4 flex-1">
+          <h4 className="text-sm font-bold md:text-lg max-md:text-lg">
             <a href={`/u/${data?.team_user?.user_handle}`}>{data?.team_user?.user_nick_name}</a>
           </h4>
-          <p className="text-xs text-gray-500 md:text-sm">
+          <p className="text-xs text-gray-500 md:text-sm max-md:text-sm">
             Posted on <span className="text-gray-50">{formatTime(Number(data?.base?.cs_created_at) * 1000, 'MMM D, YYYY HH:mm')}</span> · Latest update on{' '}
             <span className="text-gray-50">{formatTime(data?.base?.cs_updated_at * 1000, 'MMM D, YYYY')}</span>
           </p>
