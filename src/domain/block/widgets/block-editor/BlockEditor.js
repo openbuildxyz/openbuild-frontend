13b7b89fefa118ce8fd61e28af7d9c41cdac6115
Ourai Lin <ourairyu@gmail.com>
2025-01-02 22:47:16 +0800

    feat(app): add slash command for `BlockEditorWidget`

--- src/domain/block/widgets/block-editor/BlockEditor.js
+++ src/domain/block/widgets/block-editor/BlockEditor.js
@@ -15,9 +15,13 @@
  */
 
 import { isFunction } from 'lodash'
-import { EditorRoot, EditorContent } from 'novel'
+import { EditorRoot, EditorContent, EditorCommand, EditorCommandEmpty, EditorCommandList, EditorCommandItem } from 'novel'
+import { ImageResizer, handleCommandNavigation } from 'novel/extensions'
 
-import { defaultExtensions } from './helper'
+import { defaultExtensions } from './extensions'
+import { slashCommand, suggestionItems } from './slash'
+
+const extensions = [...defaultExtensions, slashCommand]
 
 function BlockEditor({ data, onChange, editable = false }) {
   const handleUpdate = ({ editor }) => {
@@ -31,15 +35,41 @@ function BlockEditor({ data, onChange, editable = false }) {
           className="relative min-h-[500px] w-full max-w-screen-lg border-muted bg-background sm:rounded-lg sm:border sm:shadow-lg"
           immediatelyRender={false}
           initialContent={data}
-          extensions={defaultExtensions}
+          extensions={extensions}
           editorProps={{
             attributes: {
               class: 'prose prose-lg dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full',
-            }
+            },
+            handleDOMEvents: {
+              keydown: (_view, event) => handleCommandNavigation(event),
+            },
           }}
           editable={editable}
           onUpdate={handleUpdate}
-        />
+          slotAfter={<ImageResizer />}
+        >
+          <EditorCommand className="z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
+            <EditorCommandEmpty className="px-2 text-muted-foreground">No results</EditorCommandEmpty>
+            <EditorCommandList>
+              {suggestionItems.map((item) => (
+                <EditorCommandItem
+                  value={item.title}
+                  onCommand={(val) => item.command(val)}
+                  className="flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent"
+                  key={item.title}
+                >
+                  <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
+                    {item.icon}
+                  </div>
+                  <div>
+                    <p className="font-medium">{item.title}</p>
+                    <p className="text-xs text-muted-foreground">{item.description}</p>
+                  </div>
+                </EditorCommandItem>
+              ))}
+            </EditorCommandList>
+          </EditorCommand>
+        </EditorContent>
       </EditorRoot>
     </div>
   ) : (
