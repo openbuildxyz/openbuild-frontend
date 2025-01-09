cce5554f695d431c51ece5d8d77680f1e32a134d
Neo Wang <wiirhan@163.com>
2025-01-09 21:49:44 +0800

    style: refine OAuth page layout and component styles
    - Adjusted LogoComponent size for better responsiveness.
    - Enhanced NoteItem component with improved text size and icon dimensions.
    - Updated spacing and padding for a more balanced layout on the OAuth page.
    - Improved text readability and alignment in authorization prompts.
    - Added footer with copyright information and logo for consistency.

--- src/domain/oauth/views/index.js
+++ src/domain/oauth/views/index.js
@@ -21,7 +21,7 @@ import Logo from 'public/images/svg/logo-black.svg';
 
 function LogoComponent({url}) {
   return (
-    <div className="relative bg-white rounded-full size-[100px]">
+    <div className="relative bg-white rounded-full size-[60px] md:size-[100px]">
       <img src={url} alt="Logo" className="" />
     </div>
   )
@@ -30,10 +30,10 @@ function LogoComponent({url}) {
 function NoteItem({title, description, icon}) {
   return (
     <div className="flex items-start gap-3">
-      <SvgIcon name={icon} size={20} />
-      <div>
-        <div className="text-sm font-bold">{title}</div>
-        <div className="text-sm leading-6 text-[#1A1A1ACC]">{description}</div>
+      <SvgIcon name={icon} size={24} className="mt-1" />
+      <div className="leading-6">
+        <div className="text-base font-bold">{title}</div>
+        <div className="text-sm text-[#1A1A1ACC]">{description}</div>
       </div>
     </div>
   )
@@ -49,10 +49,10 @@ function Oauth() {
   const name = 'Midjourney Bot'
   
   return (
-    <div className="flex min-h-screen flex-col items-center pt-[60px] p-4 font-['Nunito_Sans']">
-      <div className="relative w-[320px]">
+    <div className="flex min-h-screen flex-col items-center pt-9 md:pt-[60px] p-4 font-['Nunito_Sans'] relative">
+      <div className="relative w-[192px] md:w-[320px]">
         <div className="absolute top-1/2 left-0 w-full border-b-2 border-dashed border-[#d1d9e0]" />
-        <div className="flex items-center justify-between py-5">
+        <div className="flex items-center justify-between py-9">
           <LogoComponent url="/favicon.ico" />
           <div className="flex items-center gap-2 relative">
             <SvgIcon name='circle-check' size={20} />
@@ -60,42 +60,42 @@ function Oauth() {
           <LogoComponent url="/favicon.ico" />
         </div>
       </div>
-    <h1 className="text-[1.5rem] text-center py-4">Authorize <span>{name}</span></h1>
-    <div className="flex flex-col items-center justify-center text-lg leading-6 text-center pb-9">
-      <p className="text-[#1A1A1ACC]">
-        Log in with OpenBuild, Authorizing will redirect to
-      </p>
-      <Link url='https://midjourney.com'>
-        midjourney.com
-      </Link>
-    </div>
-    <div className="w-[500px] rounded-lg border border-gray-600 bg-white p-6 shadow-lg">
-      <div className="mb-6 space-y-4 rounded-md p-4">
-        <NoteItem icon='personal' title="Personal user data" description="Email addresses (read-only), profile information (read-only)" />
-        <NoteItem icon='international' title="Public data only" description="Limited access to your public data" />
+      <h1 className="text-[1.5rem] text-center pb-4">Authorize <span>{name}</span></h1>
+      <div className="flex flex-col items-center justify-center text-base md:text-lg leading-6 text-center pb-6 md:pb-9">
+        <p className="text-[#1A1A1ACC]">
+          Log in with OpenBuild, Authorizing will redirect to
+        </p>
+        <Link url='https://midjourney.com'>
+          midjourney.com
+        </Link>
       </div>
-      <div className="flex gap-3 border-y border-gray-600 p-6 mx-[-24px]">
-          <Button variant='outlined' className='flex-1'>
-            Cancel
+      <div className="rounded-lg border border-gray-600 bg-white p-6 shadow-lg mb-[210px]">
+        <div className="space-y-4 rounded-md mb-6">
+          <NoteItem icon='personal' title="Personal user data" description="Email addresses (read-only), profile information (read-only)" />
+          <NoteItem icon='international' title="Public data only" description="Limited access to your public data" />
+        </div>
+        <div className="flex gap-3 border-y border-gray-600 p-6 mx-[-24px]">
+            <Button variant='outlined' className='flex-1'>
+              Cancel
+            </Button>
+          <Button variant='contained' className='flex-1'>
+            Authorize
           </Button>
-        <Button variant='contained' className='flex-1'>
-          Authorize
-        </Button>
+        </div>
+        <div className='py-6 flex items-center gap-3'>
+          <SvgIcon name='lock' size={16} />
+          <span className="text-sm leading-6 text-[#1A1A1A99]">
+            Developer's <Link url='https://midjourney.com'>Privacy Policy</Link> and <Link url='https://midjourney.com'>Terms</Link> of Service
+          </span>
+        </div>
       </div>
-      <div className='py-6 flex items-center gap-3'>
-        <SvgIcon name='lock' size={16} />
-        <span className="text-[14px] leading-6 text-[#1A1A1A99]">
-          Developer's <Link url='https://midjourney.com'>Privacy Policy</Link> and <Link url='https://midjourney.com'>Terms</Link> of Service
+      <footer className='flex absolute bottom-9 gap-3 items-center'>
+        <Image className="h-4 w-auto" src={Logo} alt="" />
+        <span className="text-sm leading-[24px] opacity-60">
+          © 2023 OpenBuild, All rights reserved.
         </span>
-      </div>
+      </footer>
     </div>
-    <footer className='flex fixed bottom-9 gap-3 items-center'>
-      <Image className="h-4 w-auto" src={Logo} alt="" />
-      <span className="text-[14px] leading-[24px] opacity-60">
-        © 2023 OpenBuild, All rights reserved.
-      </span>
-    </footer>
-  </div>
   )
 }
 
