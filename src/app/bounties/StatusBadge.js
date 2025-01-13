c7a29673825f4eeddb1d429d454b1ce038321fcc
Ourai Lin <ourairyu@gmail.com>
2025-01-13 12:03:28 +0800

    fix(bounty): style detail of `StatusBadge`

--- src/app/bounties/StatusBadge.js
+++ src/app/bounties/StatusBadge.js
@@ -18,33 +18,33 @@ const badgeList = [
   {
     label: 'Recruiting',
     emoji: '🧱',
-    textColor: '#807DFB',
-    bgColor: '#807DFB',
-    borderColor: '#807DFB',
+    textColor: 'text-[#807DFB]',
+    bgColor: 'bg-[#807DFB]',
+    borderColor: 'border-[#807DFB]',
     isMatched: status => status === 3,
   },
   {
     label: 'Building',
     emoji: '🔫',
-    textColor: '#00C475',
-    bgColor: '#01DB83',
-    borderColor: '#01DB83',
+    textColor: 'text-[#00C475]',
+    bgColor: 'bg-[#01DB83]',
+    borderColor: 'border-[#01DB83]',
     isMatched: status => status > 6 && status < 24,
   },
   {
     label: 'Completed',
     emoji: '🔚',
-    textColor: '#82ADD8',
-    bgColor: '#82ADD8',
-    borderColor: '#82ADD8',
+    textColor: 'text-[#82ADD8]',
+    bgColor: 'bg-[#82ADD8]',
+    borderColor: 'border-[#82ADD8]',
     isMatched: status => status === 30,
   },
   {
     label: 'Termination',
     emoji: '🔚',
-    textColor: '#82ADD8',
-    bgColor: '#82ADD8',
-    borderColor: '#82ADD8',
+    textColor: 'text-[#82ADD8]',
+    bgColor: 'bg-[#82ADD8]',
+    borderColor: 'border-[#82ADD8]',
     isMatched: status => status === 24 || status === 20,
   },
 ]
@@ -53,11 +53,11 @@ function StatusBadge({ status }) {
   const badge = badgeList.find(({ isMatched }) => isMatched(status))
 
   return badge && (
-    <div className={`inline-flex items-center gap-2 bg-[${badge.bgColor}] bg-opacity-10 rounded-full`}>
-      <span className={`w-[24px] h-[24px] rounded-full flex items-center justify-center border-2 border-[${badge.borderColor}]`}>
+    <div className={`inline-flex items-center gap-2 p-[3px] ${badge.bgColor} bg-opacity-10 rounded-full`}>
+      <span className={`size-6 text-xs rounded-full text-center leading-[22px] border-1 ${badge.borderColor}`}>
         {badge.emoji}
       </span>
-      <span className={`text-xs font-semibold text-[${badge.textColor}] font-nunito leading-[12px] text-left pr-2`}>
+      <span className={`text-xs font-semibold ${badge.textColor} font-nunito leading-[12px] text-left pr-2`}>
         {badge.label}
       </span>
     </div>
