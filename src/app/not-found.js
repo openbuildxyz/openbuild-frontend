9f9d056b57430de973dfde4315599f088fb82755
Brendan Dash <me@aiwan.run>
2025-01-13 19:41:14 +0800

    refactor: use tailwindcss

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -18,25 +18,12 @@
 
 import Image from 'next/image'
 import BotFoundPic from 'public/images/svg/404.svg'
-import styled from 'styled-components'
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  p {
-    margin-top: 24px;
-    opacity: 0.6;
-  }
-`
 
 export default function NotFound() {
   return (
-    <Wrapper>
+    <div className='flex flex-col items-center justify-center h-screen'>
       <Image src={BotFoundPic} alt="" />
-      <p>404</p>
-    </Wrapper>
+      <p className='mt-6 opacity-60'>404</p>
+    </div>
   )
 }
