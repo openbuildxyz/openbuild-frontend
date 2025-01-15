fb67c68fcb4bda1d98ab4d7fb49e6a638b02c058
Yolo <g20180630105@gmail.com>
2025-01-15 11:34:25 +0800

    fix: 修改课程语言搜索

--- src/app/learn/[type]/TopFilters.js
+++ src/app/learn/[type]/TopFilters.js
@@ -14,20 +14,20 @@
  * limitations under the License.
  */
 
-'use client'
+'use client';
 
-import { Search } from './Search'
-import { Sort } from './Sort'
+import { Search } from './Search';
+import { Sort } from './Sort';
 
-import Image from 'next/image'
-import FeaturedIcon from 'public/images/svg/featured.svg'
-import FeaturedActiveIcon from 'public/images/svg/featured_active.svg'
-import clsx from 'clsx'
-import { ReactSelect } from '@/components/Select/ReactSelect'
-import { useMemo } from 'react'
+import Image from 'next/image';
+import FeaturedIcon from 'public/images/svg/featured.svg';
+import FeaturedActiveIcon from 'public/images/svg/featured_active.svg';
+import clsx from 'clsx';
+import { ReactSelect } from '@/components/Select/ReactSelect';
+import { useMemo, useLayoutEffect } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
-const tabStyle = 'inline-block border-gray-600 px-4 h-10 leading-10 text-gray-500 cursor-pointer'
+const tabStyle = 'inline-block border-gray-600 px-4 h-10 leading-10 text-gray-500 cursor-pointer';
 
 const langOptions = [
   {
@@ -38,36 +38,48 @@ const langOptions = [
     label: 'English',
     value: 'en',
   },
-]
+];
 
-export function TopFilters({type}) {
+const getBrowserLanguage = () => {
+  if (navigator.language) return navigator.language;
+  return 'en';
+};
+
+export function TopFilters({ type }) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
   const recommendType = useMemo(() => {
-    return searchParams?.get('recommend_type') || null
-  }, [searchParams  ])
+    return searchParams?.get('recommend_type') || null;
+  }, [searchParams]);
 
   const bodyType = useMemo(() => {
-    return searchParams?.get('body_type') || null
-  }, [searchParams  ])
+    return searchParams?.get('body_type') || null;
+  }, [searchParams]);
 
   const lang = useMemo(() => {
-    return searchParams?.get('lang') || null
-  }, [searchParams  ])
+    return searchParams?.get('lang') || null;
+  }, [searchParams]);
 
-  const changeParams = (type, vaue) => {
+  const changeParams = (type, value) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
-    if (vaue === null) {
-      params.delete(type)
+    if (value === null) {
+      params.delete(type);
     } else {
-      params.set(type, vaue)
+      params.set(type, value);
     }
-
     replace(`${pathname}?${params.toString()}`);
-  }
+  };
+
+  useLayoutEffect(() => {
+    if (localStorage.getItem('ob_userLang')) {
+      changeParams('lang', localStorage.getItem('ob_userLang'));
+    } else {
+      changeParams('lang', getBrowserLanguage() === 'zh-CN' ? 'zh' : 'en');
+    }
+  }, []);
 
   return (
     <div className="flex gap-2 items-center max-md:flex-wrap">
@@ -80,13 +92,19 @@ export function TopFilters({type}) {
               {'!border-gray' : recommendType === 'choice'}
             )}
           >
-            <Image src={FeaturedIcon} alt="" className={clsx('group-hover:hidden', { '!hidden': recommendType === 'choice'})} />
-            <Image src={FeaturedActiveIcon} alt="" className={clsx('group-hover:block hidden', { '!block': recommendType === 'choice' })} />
+            <Image
+              src={FeaturedIcon}
+              alt=""
+              className={clsx('group-hover:hidden', { '!hidden': recommendType === 'choice' })}
+            />
+            <Image
+              src={FeaturedActiveIcon}
+              alt=""
+              className={clsx('group-hover:block hidden', { '!block': recommendType === 'choice' })}
+            />
             <span className="ml-2 opacity-80 group-hover:opacity-100">Featured</span>
           </button>
-          <div
-            className="group border border-gray-600 text-sm h-10 flex items-center rounded"
-          >
+          <div className="group border border-gray-600 text-sm h-10 flex items-center rounded">
             <span
               onClick={() => changeParams('body_type', null)}
               className={clsx('rounded-l hover:!text-gray', tabStyle, { '!text-gray bg-gray-800': !bodyType })}
@@ -95,13 +113,17 @@ export function TopFilters({type}) {
             </span>
             <span
               onClick={() => changeParams('body_type', 'video')}
-              className={clsx('border-r border-l hover:!text-gray', tabStyle, { '!text-gray bg-gray-800': bodyType === 'video' })}
+              className={clsx('border-r border-l hover:!text-gray', tabStyle, {
+                '!text-gray bg-gray-800': bodyType === 'video',
+              })}
             >
               Video
             </span>
             <span
               onClick={() => changeParams('body_type', 'text')}
-              className={clsx('rounded-r hover:!text-gray', tabStyle, { '!text-gray bg-gray-800': bodyType === 'text' })}
+              className={clsx('rounded-r hover:!text-gray', tabStyle, {
+                '!text-gray bg-gray-800': bodyType === 'text',
+              })}
             >
               Text
             </span>
@@ -109,11 +131,12 @@ export function TopFilters({type}) {
           <div className="w-[140px]">
             <ReactSelect
               id="learn-order-select"
-              isClearable
-              isSearchable={false}
               value={langOptions.find(f => f.value === lang)}
               className="no-bg showDropdownIndicator w-full bg-transparent height-sm"
-              onChange={e => changeParams('lang', e ? e.value : null)}
+              onChange={e => {
+                localStorage.setItem('ob_userLang', e.value);
+                changeParams('lang', e ? e.value : null);
+              }}
               options={langOptions}
               placeholder={'Language'}
             />
@@ -125,5 +148,5 @@ export function TopFilters({type}) {
         <Sort />
       </div>
     </div>
-  )
+  );
 }
