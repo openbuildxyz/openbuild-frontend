c6b4c51231dbeca8d4ed5fcdcd6c999367f0e859
nmsn <136696700@qq.com>
2025-02-16 15:23:24 +0800

    refactor: optimize rendering logic in List and page

--- src/app/learn/[type]/page.js
+++ src/app/learn/[type]/page.js
@@ -25,38 +25,35 @@ import { Container } from './Container';
 import { ListSkeleton } from './ListSkeleton';
 // import { LearnNavBar } from './nav'
 
+const titles = {
+  courses: {
+    title: 'Open Courses',
+    desc: 'Selected open courses from technical skills to practical tasks, accessible to everyone with no cost!',
+  },
+  challenges: {
+    title: 'Challenges',
+    desc: 'Hands-on Bootcamp, Workshop or Hackerhouse, etc.',
+  },
+  career_path: {
+    title: 'Career Path',
+    desc: 'Learning paths designed for different levels of builder',
+  },
+};
 
 export default async function Page({ params, searchParams }) {
+  const titleData = titles?.[params?.type];
+
   return (
     <div>
       <div className="px-6">
-        {params.type === 'courses' && (
-          <Title
-            title="Open Courses"
-            desc="Selected open courses from technical skills to practical tasks, accessible to everyone with no cost!"
-            // link="https://forms.gle/s2tDbixtdqTU8xbp9"
-            // linkText="Click Here"
-          />
-        )}
-        {params.type === 'challenges' && (
-          <Title
-            title="Challenges"
-            desc="Hands-on Bootcamp, Workshop or Hackerhouse, etc."
-            // link="https://forms.gle/s2tDbixtdqTU8xbp9"
-            // linkText="Click Here"
-          />
-        )}
-        {params.type === 'career_path' && (
-          <Title
-            title="Career Path"
-            desc="Learning paths designed for different levels of builder"
-          />
-        )}
+        {titleData && <Title title={titleData.title} desc={titleData.desc} />}
         {/* <LearnNavBar /> */}
         <div className="relative flex max-md:overflow-x-hidden">
-          <Filter type={params.type === 'courses' ? 'open_course' : params.type}>{params.type === 'challenges' && <ChallengesFilter />}</Filter>
+          <Filter type={params.type === 'courses' ? 'open_course' : params.type}>
+            {params.type === 'challenges' && <ChallengesFilter />}
+          </Filter>
           <Suspense fallback={<ListSkeleton />}>
-            <Container type={params.type} searchParams={searchParams}  />
+            <Container type={params.type} searchParams={searchParams} />
           </Suspense>
         </div>
       </div>
