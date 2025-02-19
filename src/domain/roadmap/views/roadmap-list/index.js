90a03fbb93c9450bfb3d3f22368642b7ea2921a7
Ourai Lin <ourairyu@gmail.com>
2025-02-19 16:02:52 +0800

    refactor(app): list views of course, challenge and career path

--- src/domain/roadmap/views/roadmap-list/index.js
+++ src/domain/roadmap/views/roadmap-list/index.js
@@ -14,5 +14,4 @@
  * limitations under the License.
  */
 
-export { default as BuilderListWidget } from './widgets/builder-list';
-export { default as CardTitleWidget } from './widgets/card-title';
+export { default } from './RoadmapList';
