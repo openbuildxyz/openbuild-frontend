ee113768c708ec93795408c11166fcd2a00d8057
Brendan Dash <me@aiwan.run>
2025-03-10 23:20:14 +0800

    refactor: ts index page

--- src/app/manifest.tsx
+++ src/app/manifest.tsx
@@ -14,9 +14,11 @@
  * limitations under the License.
  */
 
+import type { MetadataRoute } from 'next';
+
 import { siteConfig } from '#/lib/site';
 
-export default function manifest() {
+export default function manifest(): MetadataRoute.Manifest {
   return {
     name: siteConfig.name,
     short_name: siteConfig.name,
