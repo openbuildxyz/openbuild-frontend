2366acab70a3c4f44cc1ec55d55d8231de746503
Ourai Lin <ourairyu@gmail.com>
2025-03-11 11:57:14 +0800

    refactor(script): optimize env checking

--- .knosys/scripts/command/env.js
+++ .knosys/scripts/command/env.js
@@ -1,60 +1,71 @@
-const fs = require('fs');
 const path = require('path');
 const semver = require('semver');
-const { resolveRootPath } = require('../helper');
+const { resolveRootPath, readData } = require('../helper');
+
+const rootPath = resolveRootPath();
+
+function checkNodeVersion(packageJson) {
+  const requiredNodeVersion = packageJson?.engines?.node;
+  const currentNodeVersion = process.version;
+
+  if (!requiredNodeVersion) {
+    console.error('No engines.node configuration found in package.json');
+    process.exit(1);
+  }
+
+  if (!semver.satisfies(currentNodeVersion, requiredNodeVersion)) {
+    console.error(`\nCurrent Node.js version ${currentNodeVersion} does not meet requirements!`);
+    console.error(`Please use Node.js version ${requiredNodeVersion}\n`);
+    process.exit(1);
+  }
+}
+
+function checkCodeValidators(packageJson) {
+  const detectionList = ['eslint', 'commitlint', 'husky', 'lint-staged'];
+  const { dependencies, devDependencies } = packageJson;
+  const allDeps = { ...dependencies, ...devDependencies };
+
+  const packagesToCheck = Object.entries(allDeps).reduce((acc, [pkg, versionSpec]) => {
+    if (detectionList.some(item => pkg.includes(item))) {
+      acc[pkg] = versionSpec;
+    }
+    return acc;
+  }, {});
+
+  let allPassed = true;
+
+  Object.entries(packagesToCheck).forEach(([pkg, expectedVersion]) => {
+    const pkgJsonPath = path.join(rootPath, 'node_modules', pkg, 'package.json');
+
+    let installedVersion;
+
+    try {
+      const pkgJson = readData(pkgJsonPath);
+      installedVersion = pkgJson.version;
+    } catch (error) {
+      console.error(`Package ${pkg} is not installed.`);
+      allPassed = false;
+      return;
+    }
+
+    if (!semver.satisfies(installedVersion, expectedVersion)) {
+      console.error(
+        `Package ${pkg} version ${installedVersion} does not meet the expected version ${expectedVersion}.`,
+      );
+      allPassed = false;
+    }
+  });
+
+  if (!allPassed) {
+    process.exit(1);
+  }
+}
 
 module.exports = {
   execute: () => {
-    const packageJsonPath = path.join(resolveRootPath(), 'package.json');
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-    const requiredNodeVersion = packageJson?.engines?.node;
-    const currentNodeVersion = process.version;
+    const packageJson = readData(path.join(rootPath, 'package.json'));
 
-    if (!requiredNodeVersion) {
-      console.error('No engines.node configuration found in package.json');
-      process.exit(1);
-    }
-
-    if (!semver.satisfies(currentNodeVersion, requiredNodeVersion)) {
-      console.error(`\nCurrent Node.js version ${currentNodeVersion} does not meet requirements!`);
-      console.error(`Please use Node.js version ${requiredNodeVersion}\n`);
-      process.exit(1);
-    }
-
-    const detectionList = ['eslint', 'commitlint', 'husky', 'lint-staged'];
-    const { dependencies, devDependencies } = packageJson;
-    const allDeps = { ...dependencies, ...devDependencies };
-
-    const packagesToCheck = Object.entries(allDeps).reduce((acc, [pkg, versionSpec]) => {
-      if (detectionList.some(item => pkg.includes(item))) {
-        acc[pkg] = versionSpec;
-      }
-      return acc;
-    }, {});
-
-    let allPassed = true;
-
-    Object.entries(packagesToCheck).forEach(([pkg, expectedVersion]) => {
-      const pkgJsonPath = path.join(process.cwd(), 'node_modules', pkg, 'package.json');
-      let installedVersion;
-      try {
-        const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
-        installedVersion = pkgJson.version;
-      } catch (error) {
-        console.error(`Package ${pkg} is not installed.`);
-        allPassed = false;
-        return;
-      }
-      if (!semver.satisfies(installedVersion, expectedVersion)) {
-        console.error(
-          `Package ${pkg} version ${installedVersion} does not meet the expected version ${expectedVersion}.`,
-        );
-        allPassed = false;
-      }
-    });
-
-    if (!allPassed) {
-      process.exit(1);
-    }
+    checkNodeVersion(packageJson);
+    checkCodeValidators(packageJson);
   },
 };
